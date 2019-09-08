f8b3254ad91a75db47885cd6c3ea01e710690a3f
Suffoquer <1161290791@qq.com>
2019-09-08 23:40:28 +0800

    Merge branch 'master' of https://github.com/Suffoquer-fang/NewsApp
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/newsapplication/Activity/NewsDetailActivity.java
    #	app/src/main/java/com/example/newsapplication/helper/MyDBOpenHelper.java
    
    WDNMD

--- app/src/androidTest/java/com/example/newsapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/newsapplication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.newsapplication;
+package com.example.Fangyan;
 
 import android.content.Context;
 
--- app/src/test/java/com/example/newsapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/newsapplication/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.newsapplication;
+package com.example.Fangyan;
 
 import org.junit.Test;
 
